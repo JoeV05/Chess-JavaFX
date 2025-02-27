1627943ae56026d2f59bcc7cc1a354502d3e49d6
Joseph <josephvinson183@gmail.com>
2025-02-27 18:13:56 +0000

    Class for bishop

---
+++ src/Bishop.java
@@ -0,0 +1,10 @@
+public class Bishop extends DirectionPiece
+{
+    public static final Direction[] DIRECTIONS = {Direction.NORTH_EAST, Direction.SOUTH_EAST, Direction.SOUTH_WEST, Direction.NORTH_WEST};
+
+    public Bishop(Colour colour, Coordinate location)
+    {
+        super(colour, location, DIRECTIONS);
+        this.type = PieceType.BISHOP;
+    }
+}
