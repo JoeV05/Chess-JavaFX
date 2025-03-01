536125c05b3b506718dc7eca1eb12fef6c75db8e
Joseph <josephvinson183@gmail.com>
2025-03-01 20:39:40 +0000

    Added vectors for movement

--- src/King.java
+++ src/King.java
@@ -7,5 +7,16 @@ public class King extends VectorPiece
         super(colour, location, null);
         this.type = PieceType.KING;
         this.sprite = new Image("./sprites/" + this.type + this.colour.toString() + ".png");
+        this.possibleMoves = new MoveVector[]
+                {
+                        new MoveVector(-1, -1, this.colour),
+                        new MoveVector(0, -1, this.colour),
+                        new MoveVector(1, -1, this.colour),
+                        new MoveVector(1, 0, this.colour),
+                        new MoveVector(1, 1, this.colour),
+                        new MoveVector(0, 1, this.colour),
+                        new MoveVector(-1, 1, this.colour),
+                        new MoveVector(-1, 0, this.colour)
+                };
     }
 }
--- src/Knight.java
+++ src/Knight.java
@@ -7,5 +7,16 @@ public class Knight extends VectorPiece
         super(colour, location, null);
         this.type = PieceType.KNIGHT;
         this.sprite = new Image("./sprites/" + this.type + this.colour + ".png");
+        this.possibleMoves = new MoveVector[]
+        {
+                new MoveVector(1, 2, this.colour),
+                new MoveVector(-1, 2, this.colour),
+                new MoveVector(-2, 1, this.colour),
+                new MoveVector(-2, -1, this.colour),
+                new MoveVector(2, 1, this.colour),
+                new MoveVector(2, -1, this.colour),
+                new MoveVector(1, -2, this.colour),
+                new MoveVector(-1, -2, this.colour)
+        };
     }
 }
