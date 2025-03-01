d4f0fe6fc8b2ed0fbb42ffd1e3f64c5201205bc5
Joseph <josephvinson183@gmail.com>
2025-03-01 01:10:32 +0000

    Added class to represent moves in some direction of some magnitude

---
+++ src/MoveDirection.java
@@ -0,0 +1,11 @@
+public class MoveDirection
+{
+    private int distance;
+    private Direction direction;
+
+    public MoveDirection(int distance, Direction direction)
+    {
+        this.distance = distance;
+        this.direction = direction;
+    }
+}
