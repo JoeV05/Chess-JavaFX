11657150b9065a8fa9db90b49f4f4a567cb16fc6
Joseph <josephvinson183@gmail.com>
2025-03-01 20:44:26 +0000

    Split fillBoard into smaller methods, set loops to use constants for board size, added move finding for vector pieces, started move finding for direction pieces

--- src/Game.java
+++ src/Game.java
@@ -1,12 +1,19 @@
 public class Game
 {
-    private static Game game;
+    public static final int BOARD_SIZE = 8;
 
-    private Piece[][] board;
+    private static Game game;
+    private final Piece[][] board;
+    private Colour currentTurn;
+    private State state;
+    private Piece selected;
 
     private Game()
     {
+        this.board = new Piece[BOARD_SIZE][BOARD_SIZE];
         this.fillBoard();
+        this.currentTurn = Colour.WHITE;
+        this.state = State.NONE_SELECTED;
     }
 
     public static Game getGame()
@@ -18,6 +25,134 @@ public class Game
         return Game.game;
     }
 
+    public Piece getSelected()
+    {
+        return this.selected;
+    }
+
+    public void handle(int x, int y)
+    {
+        Piece clickedPiece = board[y][x];
+        if (clickedPiece != null)
+        {
+            if (clickedPiece.getColour() == this.currentTurn)
+            {
+                this.selected = clickedPiece;
+                this.state = State.PIECE_SELECTED;
+            }
+        } else if (this.state == State.PIECE_SELECTED)
+        {
+            //Move the selected piece
+            System.out.println("Test text");
+        }
+    }
+
+    public MoveVector[] getPossibleVectorMoves(VectorPiece p)
+    {
+        MoveVector[] moves = p.getVectors();
+        Coordinate c = p.getLocation();
+        MoveVector[] allowedMoves = new MoveVector[moves.length];
+
+        for (int i = 0; i < moves.length; i ++)
+        {
+            MoveVector move = moves[i];
+            Coordinate newC = move.applyVector(c);
+
+            int x = newC.getX();
+            int y = newC.getY();
+
+            if ((x < BOARD_SIZE && y < BOARD_SIZE) && (x >= 0 && y >= 0) && board[y][x] == null)
+            {
+                allowedMoves[i] = move;
+            }
+        }
+        return allowedMoves;
+    }
+
+    public MoveDirection[] getPossibleDirectionalMoves(DirectionPiece p)
+    {
+        Direction[] dirs = p.getDirections();
+        MoveDirection[] possibleMoves = new MoveDirection[dirs.length];
+        for (int i = 0; i < dirs.length; i++)
+        {
+            int distance = this.distanceToBlock((Piece) p, dirs[i]);
+            if (distance > 0) {
+                MoveDirection m = new MoveDirection(distance, dirs[i]);
+                possibleMoves[i] = m;
+            }
+        }
+        return possibleMoves;
+    }
+
+    private int distanceToBlock(Piece piece, Direction dir)
+    {
+        int x = piece.getX();
+        int y = piece.getY();
+        int currentX;
+        int currentY;
+        switch (dir)
+        {
+
+            case Direction.NORTH:
+                currentY = y + 1;
+                while (currentY >= 0)
+                {
+                    if (this.board[currentY][x] != null)
+                    {
+                        return piece.getY() - currentY;
+                    }
+
+                    currentY--;
+                }
+
+            case Direction.EAST:
+                currentX = x + 1;
+                while (currentX <= 7)
+                {
+                    if (this.board[y][currentX] != null)
+                    {
+                        return currentX - x;
+                    }
+
+                    currentX++;
+                }
+
+            case Direction.SOUTH:
+                currentY = y - 1;
+                while (currentY <= 7)
+                {
+                    if (this.board[currentY][x] != null)
+                    {
+                        return piece.getY() - currentY;
+                    }
+
+                    currentY--;
+                }
+
+            case Direction.WEST:
+                currentX = x + 1;
+                while (currentX >= 0)
+                {
+                    if (this.board[y][currentX] != null)
+                    {
+                        return x - currentX;
+                    }
+
+                    currentX--;
+                }
+
+            case Direction.NORTH_EAST:
+            case Direction.SOUTH_EAST:
+            case Direction.SOUTH_WEST:
+            case Direction.NORTH_WEST:
+                return 0;
+
+            default:
+                throw new IllegalArgumentException("Invalid direction passed in: " + dir);
+        }
+    }
+
+
     public Piece[][] getBoard()
     {
         return this.board;
@@ -25,9 +160,9 @@ public class Game
 
     public void printBoard()
     {
-        for (int i = 0; i < 8; i++)
+        for (int i = 0; i < BOARD_SIZE; i++)
         {
-            for (int j = 0; j < 8; j++)
+            for (int j = 0; j < BOARD_SIZE; j++)
             {
                 if (this.board[i][j] != null)
                 {
@@ -44,35 +179,58 @@ public class Game
 
     private void fillBoard()
     {
-        if (this.board == null)
-        {
-            this.board = new Piece[8][8];
-        }
+        this.addPawns();
+        this.addRooks();
+        this.addKnights();
+        this.addBishops();
+        this.addKings();
+        this.addQueens();
 
-        for (int i = 0; i < 8; i++)
+        // TODO - debug pieces, remove at some point
+        board[4][4] = new Queen(Colour.WHITE, new Coordinate(4, 4));
+    }
+
+    private void addPawns()
+    {
+        for (int i = 0; i < BOARD_SIZE; i++)
         {
             this.board[1][i] = new Pawn(Colour.BLACK, new Coordinate(i, 1));
             this.board[6][i] = new Pawn(Colour.WHITE, new Coordinate(i, 6));
         }
+    }
 
+    private void addRooks()
+    {
         this.board[0][0] = new Rook(Colour.BLACK, new Coordinate(0, 0));
         this.board[0][7] = new Rook(Colour.BLACK, new Coordinate(7, 0));
         this.board[7][0] = new Rook(Colour.WHITE, new Coordinate(0, 7));
         this.board[7][7] = new Rook(Colour.WHITE, new Coordinate(7, 7));
+    }
 
+    private void addKnights()
+    {
         this.board[0][1] = new Knight(Colour.BLACK, new Coordinate(1, 0));
         this.board[0][6] = new Knight(Colour.BLACK, new Coordinate(6, 0));
         this.board[7][1] = new Knight(Colour.WHITE, new Coordinate(1, 7));
         this.board[7][6] = new Knight(Colour.WHITE, new Coordinate(6, 7));
+    }
 
+    private void addBishops()
+    {
         this.board[0][2] = new Bishop(Colour.BLACK, new Coordinate(2, 0));
         this.board[0][5] = new Bishop(Colour.BLACK, new Coordinate(5, 0));
         this.board[7][2] = new Bishop(Colour.WHITE, new Coordinate(2, 7));
         this.board[7][5] = new Bishop(Colour.WHITE, new Coordinate(5, 7));
+    }
 
+    private void addKings()
+    {
         this.board[0][4] = new King(Colour.BLACK, new Coordinate(4, 0));
         this.board[7][3] = new King(Colour.WHITE, new Coordinate(3, 7));
+    }
 
+    private void addQueens()
+    {
         this.board[0][3] = new Queen(Colour.BLACK, new Coordinate(3, 0));
         this.board[7][4] = new Queen(Colour.WHITE, new Coordinate(4, 7));
     }
