bd5e672c3656ff9c46c1c8db723c9cf4d42bc93d
Joseph <josephvinson183@gmail.com>
2025-03-02 17:08:54 +0000

    Removed unnecessary method

--- src/VectorPiece.java
+++ src/VectorPiece.java
@@ -10,11 +10,6 @@ public abstract class VectorPiece extends Piece
         this.possibleMoves = possibleMoves;
     }
 
-    public boolean validateMove(MoveVector move)
-    {
-        return Arrays.asList(possibleMoves).contains(move);
-    }
-
     public void move(MoveVector move)
     {
         this.location = move.applyVector(this.location);
